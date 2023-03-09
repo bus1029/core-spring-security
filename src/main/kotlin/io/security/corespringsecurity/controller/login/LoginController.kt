0602cbc5d86fe7b53c5ae3e2bfa5b39aba8daf78
Seokhyun Bae <bus1029@gmail.com>
2023-03-09 13:15:26 +0900

    feat: 로그아웃 기능 추가
    - top.html에 타임리프에서 제공하는 isAnonymous()와 
    isAuthenticated() 메소드를 사용해서 인증 여부에 따라 다르게 
    요소들을 출력한다.
    - <a> 태그를 사용해서 로그아웃을 요청하기 때문에 GET 방식으로
    /logout 주소로 데이터를 받기 위해 LoginController에 /logout 
    API를 추가한다.
    --- SecurityContextHolder에서 Authentication 객체를 꺼낸뒤 
    직접 SecurityContextLogoutHandler를 생성하여 로그아웃 
    처리를 한다.

--- src/main/kotlin/io/security/corespringsecurity/controller/login/LoginController.kt
+++ src/main/kotlin/io/security/corespringsecurity/controller/login/LoginController.kt
@@ -1,12 +1,28 @@
 package io.security.corespringsecurity.controller.login
 
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 class LoginController {
   @GetMapping("/login")
   fun login(): String {
     return "user/login/login"
+  }
+
+  @GetMapping("/logout")
+  fun logout(
+    request: HttpServletRequest,
+    response: HttpServletResponse): String {
+    val authentication = SecurityContextHolder.getContext().authentication
+    authentication?.let {
+      SecurityContextLogoutHandler().logout(request, response, authentication)
+    }
+
+    return "redirect:/login"
   }
 }
